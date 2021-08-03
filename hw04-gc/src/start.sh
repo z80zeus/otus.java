4df12aa84de67efe492c40cf6188979b0eb9c01d
Vladimir Lazarev <solock@mail.ru>
2021-08-03 18:58:15 +0300

    HW04-GC last version.

--- hw04-gc/src/start.sh
+++ hw04-gc/src/start.sh
@@ -3,11 +3,10 @@
 LOGDIR=/tmp/java.logs
 mkdir $LOGDIR > /dev/null 2>&1
 
-HEAPSIZE="2048"
+HEAPSIZE="256"
 HEAPSTEP="128"
-#MEMLIMIT="600"
-MEMLIMIT="4100"
-TESTS=3
+MEMLIMIT="2100"
+TESTS=10
 
 while [[ $HEAPSIZE -lt $MEMLIMIT ]]; do
 
@@ -18,25 +17,10 @@ PARAM="\
 -XX:+HeapDumpOnOutOfMemoryError \
 -XX:HeapDumpPath=$LOGDIR/heapdump.hprof \
 -Xlog:gc=debug:file=$LOGDIR/gc-%p-%t.log:tags,uptime,time,level:filecount=5,filesize=10m \
--XX:+UseParallelGC -XX:+UseLargePages
+-XX:+UseParallelGC -XX:-UseParallelOldGC -XX:ParallelGCThreads=2
 "
 
-#PARAM="-server -XX:+UseParallelGC -XX:+UseLargePages -Xmn4g  -Xms8g -Xmx8g"
-#PARAM="-XX:+UseG1GC -Xmn4g  -Xms8g -Xmx8g"
-#PARAM="-XX:+UseSerialGC -XX:+UseParallelOldGC -Xmn4g  -Xms8g -Xmx8g"
-#PARAM="-XX:+UseConcMarkSweepGC -Xmn4g  -Xms8g -Xmx8g"
-
-#-XX:+UseParallelGC
-#-XX:+UseG1GC \
-#-XX:+UseSerialGC
-#-XX:-UseParallelOldGC
-# -XX:+UnlockExperimentalVMOptions -XX:+UseZGC
-
-#-XX:NewRatio=1
-#-XX:NewSize=$HEAPSIZE
-#-XX:MaxNewSize=$HEAPSIZE
-#-Xmn:256m
-#-XX:MaxGCPauseMillis=1
+#-XX:+UseG1GC
 
 ACCUM=0
 for ((i=0; i<$TESTS; i++)); do
