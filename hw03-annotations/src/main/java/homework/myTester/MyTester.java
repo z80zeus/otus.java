ebf149daf652434987fdfbdc050ca2b543e5da0e
Vladimir Lazarev <solock@mail.ru>
2021-08-04 17:39:32 +0300

    Annotations & empty RunTest.

--- hw03-annotations/src/main/java/homework/myTester/MyTester.java
+++ hw03-annotations/src/main/java/homework/myTester/MyTester.java
@@ -1,21 +1,7 @@
 package homework.myTester;
 
-import java.lang.annotation.*;
-
-@Retention(value=RetentionPolicy.RUNTIME)
-@Target(value=ElementType.METHOD)
-public @interface Before {}
-
-@Retention(value=RetentionPolicy.RUNTIME)
-@Target(value=ElementType.METHOD)
-public @interface After {}
-
-@Retention(value=RetentionPolicy.RUNTIME)
-@Target(value=ElementType.METHOD)
-public @interface Test {}
-
 public class MyTester {
-    public static void runTest(String className) throws ClassNotFoundException {
-        Class clazz = Class.forName(className);
+    public static void runTest(String className) { // throws ClassNotFoundException {
+        // Class clazz = Class.forName(className);
     }
 }
--- hw03-annotations/src/test/java/homework/MyTest.java
+++ hw03-annotations/src/test/java/homework/MyTest.java
@@ -1,6 +1,6 @@
 package homework;
 
-import homework.class4test.TestIt;
+//import homework.class4test.TestIt;
 import homework.myTester.*;
 
 public class MyTest {
@@ -14,19 +14,19 @@ public class MyTest {
 
     @Test
     void test1() {
-        TestIt testIt = new TestIt();
-        testIt.intField = 1;
+//        TestIt testIt = new TestIt();
+//        testIt.intField = 1;
     }
 
     @Test
     void test2() {
-        TestIt testIt = new TestIt();
-        testIt.intField = Integer.MAX_VALUE;
+//        TestIt testIt = new TestIt();
+//        testIt.intField = Integer.MAX_VALUE;
     }
 
     @Test
     void test3() {
-        TestIt testIt = new TestIt();
-        testIt.intField = Integer.MIN_VALUE;
+//        TestIt testIt = new TestIt();
+//        testIt.intField = Integer.MIN_VALUE;
     }
 }
