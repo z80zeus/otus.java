9f556d7020689c89b9d9f95cdab09df35d88f198
Vladimir Lazarev <solock@mail.ru>
2021-10-25 23:20:06 +0300

    Initial commit.

---
+++ hw08-io/src/main/java/ru/otus/dataprocessor/FileLoader.java
@@ -0,0 +1,17 @@
+package ru.otus.dataprocessor;
+
+import ru.otus.model.Measurement;
+
+import java.util.List;
+
+public class FileLoader implements Loader {
+
+    public FileLoader(String fileName) {
+    }
+
+    @Override
+    public List<Measurement> load() {
+        //читает файл, парсит и возвращает результат
+        return null;
+    }
+}
